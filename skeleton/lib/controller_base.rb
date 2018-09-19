20c313ee5abe7fcb0bdf95f3119c4f636881054d
joseph-rato <joseph.rato@gmail.com>
2018-09-19 16:38:32 -0700

    Finish phase 3 of rails lite

---
+++ skeleton/lib/controller_base.rb
@@ -0,0 +1,60 @@
+require 'active_support'
+require 'active_support/core_ext'
+require 'erb'
+require_relative './session'
+require 'byebug'
+
+class ControllerBase
+  attr_reader :req, :res, :params
+
+  # Setup the controller
+  def initialize(req, res)
+    @req = req 
+    @res = res
+  end
+
+  # Helper method to alias @already_built_response
+  def already_built_response?
+    @already_built_response 
+  
+  end
+
+  # Set the response status code and header
+  def redirect_to(url)
+    raise "get your sh*t together son" if @already_built_response 
+    @res['location'] = url
+    @res.status = 302
+    @already_built_response = true
+  end
+
+  # Populate the response with content.
+  # Set the response's content type to the given type.
+  # Raise an error if the developer tries to double render.
+  def render_content(content, content_type)
+    # debugger
+    raise "get your sh*t together son" if @already_built_response 
+    @res['Content-Type'] = content_type
+    @res.write content
+    @already_built_response = true
+  end
+
+  # use ERB and binding to evaluate templates
+  # pass the rendered html to render_content
+  def render(template_name)
+    dir_path = File.dirname(__FILE__)
+    debugger
+    template_path = File.join(dir_path,"../views/#{self.class.to_s.underscore}/#{template_name}.html.erb")
+    template_code = File.read(template_path)
+    render_content(ERB.new(template_code).result(binding), 'text/html')                                
+    
+  end
+
+  # method exposing a `Session` object
+  def session
+  end
+
+  # use this with the router to call action_name (:index, :show, :create...)
+  def invoke_action(name)
+  end
+end
+
